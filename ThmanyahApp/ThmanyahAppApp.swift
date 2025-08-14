bf9bbe824ec519bd48e5c63b0e5593e68927d2d2
Ahmed Mahrous <ahmedmahros425@gmail.com>
2025-08-14 18:15:27 +0300

    Initial Commit

---
+++ ThmanyahApp/ThmanyahAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahAppApp.swift
+//  ThmanyahApp
+//
+//  Created by Ahmed Mahrous on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThmanyahAppTests/ThmanyahAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahAppTests.swift
+//  ThmanyahAppTests
+//
+//  Created by Ahmed Mahrous on 14/08/2025.
+//
+
+import Testing
+@testable import ThmanyahApp
+
+struct ThmanyahAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
